36843576963076576e9d657ffe39758a5bb8ab35
Ian <53088991+ian40491777@users.noreply.github.com>
2025-05-14 22:32:04 -0300

    arquitectura y luz

--- Assets/Scripts/Player/MouseSensibilityUI.cs
+++ Assets/Scripts/Player/MouseSensibilityUI.cs
@@ -14,7 +14,7 @@ public class MouseSensibilityUI : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         cameraController = GetComponent<PlayerCam>();
-        sensibilitySlider.minValue = 10f;
+        sensibilitySlider.minValue = 0.5f;
         sensibilitySlider.maxValue = 1000f;
         cameraController.sens = 500;
         //sensibilidadSlider.onValueChanged.AddListener(UpdateSensitivity);
