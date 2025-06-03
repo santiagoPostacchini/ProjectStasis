31c1e002acb54e8f7b208dacb48cfd39520737a9
Ian <53088991+ian40491777@users.noreply.github.com>
2025-06-03 19:54:12 -0300

    animator controller y animaciones

--- Assets/Scripts/Player/ArmAnimationHandler.cs
+++ Assets/Scripts/Player/ArmAnimationHandler.cs
@@ -1,3 +1,81 @@
+﻿//using Events;
+//using UnityEngine;
+
+//namespace Player
+//{
+//    public class ArmAnimationHandler : MonoBehaviour
+//    {
+//        [SerializeField] private Animator armAnimator;
+
+//        private readonly int _jumpHash = Animator.StringToHash("Jump");
+//        private readonly int _crouchHash = Animator.StringToHash("Crouch");
+//        private readonly int _landHash = Animator.StringToHash("Land");
+//        private readonly int _grabHash = Animator.StringToHash("Grab");
+//        private readonly int _dropHash = Animator.StringToHash("Drop");
+//        private readonly int _throwHash = Animator.StringToHash("Throw");
+//        private readonly int _shotHash = Animator.StringToHash("Shot");
+//        private readonly int _idleHash = Animator.StringToHash("Idle");
+
+//        private void OnEnable()
+//        {
+//            EventManager.Subscribe("OnJump", HandleEvent);
+//            EventManager.Subscribe("OnLand", HandleEvent);
+//            EventManager.Subscribe("OnCrouchEnter", HandleEvent);
+//            EventManager.Subscribe("OnCrouchExit", HandleEvent);
+//            EventManager.Subscribe("OnObjectGrab", HandleEvent);
+//            EventManager.Subscribe("OnObjectDrop", HandleEvent);
+//            EventManager.Subscribe("OnObjectThrow", HandleEvent);
+//        }
+
+//        private void OnDisable()
+//        {
+//            EventManager.Unsubscribe("OnJump", HandleEvent);
+//            EventManager.Unsubscribe("OnLand", HandleEvent);
+//            EventManager.Unsubscribe("OnCrouchEnter", HandleEvent);
+//            EventManager.Unsubscribe("OnCrouchExit", HandleEvent);
+//            EventManager.Unsubscribe("OnObjectGrab", HandleEvent);
+//            EventManager.Unsubscribe("OnObjectDrop", HandleEvent);
+//            EventManager.Unsubscribe("OnObjectThrow", HandleEvent);
+//        }
+
+//        private void HandleEvent(string eventName, GameObject sender)
+//        {
+//            if (sender != transform.root.gameObject) 
+//                return;
+
+//            switch (eventName)
+//            {
+//                case "OnJump":
+//                    armAnimator.SetTrigger(_jumpHash);
+//                    break;
+//                case "OnShot":
+//                    armAnimator.SetTrigger(_shotHash);
+//                    break;
+//                case "OnLand":
+//                    armAnimator.SetTrigger(_landHash);
+//                    break;
+//                case "OnIdle":
+//                    armAnimator.SetTrigger(_idleHash);
+//                    break;
+//                case "OnCrouchEnter":
+//                    armAnimator.SetBool(_crouchHash, true);
+//                    break;
+//                case "OnCrouchExit":
+//                    armAnimator.SetBool(_crouchHash, false);
+//                    break;
+//                case "OnObjectGrab":
+//                    armAnimator.SetTrigger(_grabHash);
+//                    break;
+//                case "OnObjectDrop":
+//                    armAnimator.SetTrigger(_dropHash);
+//                    break;
+//                case "OnObjectThrow":
+//                    armAnimator.SetTrigger(_throwHash);
+//                    break;
+//            }
+//        }
+//    }
+//}
 using Events;
 using UnityEngine;
 
@@ -6,7 +84,8 @@ namespace Player
     public class ArmAnimationHandler : MonoBehaviour
     {
         [SerializeField] private Animator armAnimator;
-        
+
+        // Hashes existentes
         private readonly int _jumpHash = Animator.StringToHash("Jump");
         private readonly int _crouchHash = Animator.StringToHash("Crouch");
         private readonly int _landHash = Animator.StringToHash("Land");
@@ -15,6 +94,20 @@ namespace Player
         private readonly int _throwHash = Animator.StringToHash("Throw");
         private readonly int _shotHash = Animator.StringToHash("Shot");
         private readonly int _idleHash = Animator.StringToHash("Idle");
+
+        // ① Nuevo hash para el parámetro Speed (float)
+        private readonly int _speedHash = Animator.StringToHash("Speed");
+
+        // Referencia al Player que contiene CurrentSpeed
+        private Player _player;
+
+        private void Awake()
+        {
+            // Buscamos el componente Player en la misma raíz
+            _player = transform.root.GetComponent<Player>();
+            if (_player == null)
+                Debug.LogWarning("ArmAnimationHandler: no se encontró componente Player en la raíz.");
+        }
 
         private void OnEnable()
         {
@@ -25,6 +118,8 @@ namespace Player
             EventManager.Subscribe("OnObjectGrab", HandleEvent);
             EventManager.Subscribe("OnObjectDrop", HandleEvent);
             EventManager.Subscribe("OnObjectThrow", HandleEvent);
+            EventManager.Subscribe("OnShot", HandleEvent);
+            EventManager.Subscribe("OnIdle", HandleEvent);
         }
 
         private void OnDisable()
@@ -36,11 +131,23 @@ namespace Player
             EventManager.Unsubscribe("OnObjectGrab", HandleEvent);
             EventManager.Unsubscribe("OnObjectDrop", HandleEvent);
             EventManager.Unsubscribe("OnObjectThrow", HandleEvent);
+            EventManager.Unsubscribe("OnShot", HandleEvent);
+            EventManager.Unsubscribe("OnIdle", HandleEvent);
+        }
+
+        private void Update()
+        {
+            if (_player != null)
+            {
+                // ② Cada frame enviamos el valor float "Speed" al Animator
+                float velocidadActual = _player.CurrentSpeed;
+                armAnimator.SetFloat(_speedHash, velocidadActual);
+            }
         }
 
         private void HandleEvent(string eventName, GameObject sender)
         {
-            if (sender != transform.root.gameObject) 
+            if (sender != transform.root.gameObject)
                 return;
 
             switch (eventName)
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,5 +1,221 @@
+﻿//using UnityEngine;
+//using Events;
+//namespace Player
+//{
+//    public class Player : MonoBehaviour
+//    {
+//        [Header("Movement")]
+//        [SerializeField] private float sprintSpeed = 7f;
+//        [SerializeField] private float crouchSpeed = 3f;
+//        [SerializeField] private float acceleration = 20f;
+//        [SerializeField] private float deceleration = 30f;
+//        [SerializeField] private float airControlMultiplier = 0.4f;
+
+//        [Header("Jumping")]
+//        [SerializeField] private float jumpForce = 8f;
+//        [SerializeField] private float jumpCooldown = 0.25f;
+//        [SerializeField] private float gravity = -18f;
+//        private bool _readyToJump = true;
+
+//        [Header("Crouch")]
+//        [SerializeField] private float standHeight = 2f;
+//        [SerializeField] private float crouchHeight = 1f;
+//        [SerializeField] private float standCenterY;
+//        [SerializeField] private float crouchCenterY = 0.5f;
+//        [SerializeField] private float eyeOffset = 0.6f;
+//        [SerializeField] private Transform cameraTransform;
+//        [SerializeField, Range(0.01f, 0.2f)] private float crouchSmoothTime = 0.08f;
+
+//        private float _crouchTargetHeight = 2f;
+//        private float _crouchTargetCenterY;
+//        private float _heightSmoothVelocity;
+//        private float _centerYSmoothVelocity;
+
+//        [SerializeField] private Transform orientation;
+
+//        private float _horizontalInput;
+//        private float _verticalInput;
+//        private Vector3 _moveDirection;
+//        private Vector3 _currentVelocity;
+//        private float _verticalVelocity;
+
+//        private CharacterController _controller;
+//        private Coroutine _currentCrouchRoutine;
+
+//        public enum MovementState { Sprinting, Crouching, Air }
+//        public MovementState state;
+//        public bool IsAtMinSpeed { get; private set; }
+//        public bool IsIdle { get; private set; }
+
+//        private bool _wasGrounded;
+//        private bool _isCrouching;
+
+//        private void Start()
+//        {
+//            _controller = GetComponent<CharacterController>();
+//            UpdateCameraHeight();
+//        }
+
+
+//        private void Update()
+//        {
+//            HandleInput();
+//            StateHandler();
+//            HandleJump();
+//            ApplyGravity();
+//            HandleMovement();
+//            HandleCrouchTransition();
+
+//            CheckGroundedEvents();
+//            CheckCrouchEvents();
+//        }
+//        private void HandleInput()
+//        {
+//            _horizontalInput = Input.GetAxisRaw("Horizontal");
+//            _verticalInput = Input.GetAxisRaw("Vertical");
+
+//            if (Input.GetKeyDown(KeyCode.LeftControl))
+//            {
+//                _crouchTargetHeight = crouchHeight;
+//                _crouchTargetCenterY = crouchCenterY;
+//            }
+//            else if (Input.GetKeyUp(KeyCode.LeftControl))
+//            {
+//                _crouchTargetHeight = standHeight;
+//                _crouchTargetCenterY = standCenterY;
+//            }
+//        }
+
+//        private void HandleMovement()
+//        {
+//            _moveDirection = (orientation.forward * _verticalInput + orientation.right * _horizontalInput).normalized;
+//            float targetSpeed = (state == MovementState.Crouching) ? crouchSpeed : sprintSpeed;
+//            Vector3 desiredVelocity = _moveDirection * targetSpeed;
+
+//            var flatCurrentVel = new Vector3(_currentVelocity.x, 0, _currentVelocity.z);
+//            IsAtMinSpeed = flatCurrentVel.magnitude >= targetSpeed * 0.3f;
+//            IsIdle = flatCurrentVel.magnitude <= 0.1f;
+
+//            if (IsIdle)
+//            {
+//                EventManager.TriggerEvent("OnIdle", gameObject);
+//            }
+
+//            float effectiveAcceleration = acceleration;
+//            float effectiveDeceleration = deceleration;
+//            if (!_controller.isGrounded)
+//            {
+//                effectiveAcceleration *= airControlMultiplier;
+//                effectiveDeceleration *= airControlMultiplier;
+//            }
+
+//            flatCurrentVel = new Vector3(_currentVelocity.x, 0, _currentVelocity.z);
+//            var flatDesiredVel = new Vector3(desiredVelocity.x, 0, desiredVelocity.z);
+
+//            flatCurrentVel = _moveDirection.magnitude > 0.1f ? Vector3.MoveTowards(flatCurrentVel, flatDesiredVel, effectiveAcceleration * Time.deltaTime) : Vector3.MoveTowards(flatCurrentVel, Vector3.zero, effectiveDeceleration * Time.deltaTime);
+
+//            _currentVelocity = new Vector3(flatCurrentVel.x, _currentVelocity.y, flatCurrentVel.z);
+
+//            Vector3 finalVelocity = new Vector3(flatCurrentVel.x, _verticalVelocity, flatCurrentVel.z);
+//            _controller.Move(finalVelocity * Time.deltaTime);
+//        }
+
+
+//        private void HandleJump()
+//        {
+//            if (_controller.isGrounded && _verticalVelocity < 0)
+//                _verticalVelocity = -2f;
+
+//            if (Input.GetKeyDown(KeyCode.Space) && _controller.isGrounded && state != MovementState.Crouching && _readyToJump)
+//            {
+//                _verticalVelocity = jumpForce;
+//                _readyToJump = false;
+//                Invoke(nameof(ResetJump), jumpCooldown);
+
+//                EventManager.TriggerEvent("OnJump", gameObject);
+//            }
+//        }
+
+
+//        private void ApplyGravity()
+//        {
+//            _verticalVelocity += gravity * Time.deltaTime;
+//        }
+
+//        private void HandleCrouchTransition()
+//        {
+//            _controller.height = Mathf.SmoothDamp(
+//                _controller.height,
+//                _crouchTargetHeight,
+//                ref _heightSmoothVelocity,
+//                crouchSmoothTime
+//            );
+
+//            _controller.center = new Vector3(
+//                0f,
+//                Mathf.SmoothDamp(
+//                    _controller.center.y,
+//                    _crouchTargetCenterY,
+//                    ref _centerYSmoothVelocity,
+//                    crouchSmoothTime
+//                ),
+//                0f
+//            );
+
+//            UpdateCameraHeight();
+//        }
+
+//        private void UpdateCameraHeight()
+//        {
+//            cameraTransform.localPosition = new Vector3(
+//                cameraTransform.localPosition.x,
+//                _controller.center.y + eyeOffset,
+//                cameraTransform.localPosition.z
+//            );
+//        }
+
+//        private void StateHandler()
+//        {
+//            if (Input.GetKey(KeyCode.LeftControl))
+//                state = MovementState.Crouching;
+//            else if (_controller.isGrounded)
+//                state = MovementState.Sprinting;
+//            else
+//                state = MovementState.Air;
+//        }
+
+//        private void ResetJump()
+//        {
+//            _readyToJump = true;
+//            EventManager.TriggerEvent("OnIdle", gameObject);
+//        }
+
+//        private void CheckGroundedEvents()
+//        {
+//            if (_controller.isGrounded && !_wasGrounded)
+//                EventManager.TriggerEvent("OnLand", gameObject);
+
+//            _wasGrounded = _controller.isGrounded;
+//        }
+
+//        private void CheckCrouchEvents()
+//        {
+//            bool currentlyCrouching = state == MovementState.Crouching;
+
+//            if (currentlyCrouching && !_isCrouching)
+//                EventManager.TriggerEvent("OnCrouchEnter", gameObject);
+//            else if (!currentlyCrouching && _isCrouching)
+//                EventManager.TriggerEvent("OnCrouchExit", gameObject);
+
+//            _isCrouching = currentlyCrouching;
+//        }
+//    }
+//}
+
+
 using UnityEngine;
 using Events;
+
 namespace Player
 {
     public class Player : MonoBehaviour
@@ -16,22 +232,23 @@ namespace Player
         [SerializeField] private float jumpCooldown = 0.25f;
         [SerializeField] private float gravity = -18f;
         private bool _readyToJump = true;
-        
+
         [Header("Crouch")]
         [SerializeField] private float standHeight = 2f;
         [SerializeField] private float crouchHeight = 1f;
         [SerializeField] private float standCenterY;
         [SerializeField] private float crouchCenterY = 0.5f;
         [SerializeField] private float eyeOffset = 0.6f;
-        [SerializeField] private Transform cameraTransform;
         [SerializeField, Range(0.01f, 0.2f)] private float crouchSmoothTime = 0.08f;
-        
+
         private float _crouchTargetHeight = 2f;
         private float _crouchTargetCenterY;
         private float _heightSmoothVelocity;
         private float _centerYSmoothVelocity;
-        
+
         [SerializeField] private Transform orientation;
+        [SerializeField] private Transform cameraTransform;
+
 
         private float _horizontalInput;
         private float _verticalInput;
@@ -46,7 +263,18 @@ namespace Player
         public MovementState state;
         public bool IsAtMinSpeed { get; private set; }
         public bool IsIdle { get; private set; }
-        
+
+        // ① Propiedad pública para exponer la velocidad horizontal
+        public float CurrentSpeed
+        {
+            get
+            {
+                // Solo la componente XZ de _currentVelocity
+                Vector3 flatVel = new Vector3(_currentVelocity.x, 0f, _currentVelocity.z);
+                return flatVel.magnitude;
+            }
+        }
+
         private bool _wasGrounded;
         private bool _isCrouching;
 
@@ -56,7 +284,6 @@ namespace Player
             UpdateCameraHeight();
         }
 
-        
         private void Update()
         {
             HandleInput();
@@ -69,6 +296,7 @@ namespace Player
             CheckGroundedEvents();
             CheckCrouchEvents();
         }
+
         private void HandleInput()
         {
             _horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -91,8 +319,8 @@ namespace Player
             _moveDirection = (orientation.forward * _verticalInput + orientation.right * _horizontalInput).normalized;
             float targetSpeed = (state == MovementState.Crouching) ? crouchSpeed : sprintSpeed;
             Vector3 desiredVelocity = _moveDirection * targetSpeed;
-            
-            var flatCurrentVel = new Vector3(_currentVelocity.x, 0, _currentVelocity.z);
+
+            var flatCurrentVel = new Vector3(_currentVelocity.x, 0f, _currentVelocity.z);
             IsAtMinSpeed = flatCurrentVel.magnitude >= targetSpeed * 0.3f;
             IsIdle = flatCurrentVel.magnitude <= 0.1f;
 
@@ -100,7 +328,7 @@ namespace Player
             {
                 EventManager.TriggerEvent("OnIdle", gameObject);
             }
-            
+
             float effectiveAcceleration = acceleration;
             float effectiveDeceleration = deceleration;
             if (!_controller.isGrounded)
@@ -108,18 +336,33 @@ namespace Player
                 effectiveAcceleration *= airControlMultiplier;
                 effectiveDeceleration *= airControlMultiplier;
             }
-            
-            flatCurrentVel = new Vector3(_currentVelocity.x, 0, _currentVelocity.z);
-            var flatDesiredVel = new Vector3(desiredVelocity.x, 0, desiredVelocity.z);
 
-            flatCurrentVel = _moveDirection.magnitude > 0.1f ? Vector3.MoveTowards(flatCurrentVel, flatDesiredVel, effectiveAcceleration * Time.deltaTime) : Vector3.MoveTowards(flatCurrentVel, Vector3.zero, effectiveDeceleration * Time.deltaTime);
+            flatCurrentVel = new Vector3(_currentVelocity.x, 0f, _currentVelocity.z);
+            var flatDesiredVel = new Vector3(desiredVelocity.x, 0f, desiredVelocity.z);
+
+            // Lerp o MoveTowards según input
+            if (_moveDirection.magnitude > 0.1f)
+            {
+                flatCurrentVel = Vector3.MoveTowards(
+                    flatCurrentVel,
+                    flatDesiredVel,
+                    effectiveAcceleration * Time.deltaTime
+                );
+            }
+            else
+            {
+                flatCurrentVel = Vector3.MoveTowards(
+                    flatCurrentVel,
+                    Vector3.zero,
+                    effectiveDeceleration * Time.deltaTime
+                );
+            }
 
             _currentVelocity = new Vector3(flatCurrentVel.x, _currentVelocity.y, flatCurrentVel.z);
-            
+
             Vector3 finalVelocity = new Vector3(flatCurrentVel.x, _verticalVelocity, flatCurrentVel.z);
             _controller.Move(finalVelocity * Time.deltaTime);
         }
-
 
         private void HandleJump()
         {
@@ -135,7 +378,6 @@ namespace Player
                 EventManager.TriggerEvent("OnJump", gameObject);
             }
         }
-
 
         private void ApplyGravity()
         {
@@ -189,7 +431,7 @@ namespace Player
             _readyToJump = true;
             EventManager.TriggerEvent("OnIdle", gameObject);
         }
-        
+
         private void CheckGroundedEvents()
         {
             if (_controller.isGrounded && !_wasGrounded)
@@ -197,7 +439,7 @@ namespace Player
 
             _wasGrounded = _controller.isGrounded;
         }
-        
+
         private void CheckCrouchEvents()
         {
             bool currentlyCrouching = state == MovementState.Crouching;
