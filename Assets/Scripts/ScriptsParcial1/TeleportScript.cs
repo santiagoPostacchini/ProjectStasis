05291270779365c866035a32c131089ef2eed073
LeandroBottacinEzequiel <leobottacin2@gmail.com>
2025-06-18 20:36:04 -0300

    Cambios

--- Assets/Scripts/ScriptsParcial1/TeleportScript.cs
+++ Assets/Scripts/ScriptsParcial1/TeleportScript.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class TeleportScript : MonoBehaviour
 {
-    [SerializeField] private Transform pos;
+    [SerializeField] private Transform pos1;
+    [SerializeField] private Transform pos2;
+    [SerializeField] private Transform pos3;
+    [SerializeField] private Transform pos4;
     [SerializeField] private Player.Player player;
 
     void Update()
@@ -12,7 +15,22 @@ public class TeleportScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             if (player == null) return;
-            player.transform.position = pos.transform.position;
+            player.transform.position = pos1.transform.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (player == null) return;
+            player.transform.position = pos2.transform.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (player == null) return;
+            player.transform.position = pos3.transform.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            if (player == null) return;
+            player.transform.position = pos4.transform.position;
         }
     }
 }
